4683a5b83541295e7eee6e0038ea454dade8458d
Mintesnot <mintesnotkasu@gmail.com>
2024-05-18 11:25:00 +0300

    Initial Commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet("GetProducts")]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            try
+            {
+                var products = await _storeContext.Products.ToListAsync();
+
+                return Ok(products);
+
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            }     
+           
+        }
+        
+
+        [HttpGet("GetProductById/{id}")]
+        public async Task<Product> GetProductById(int id)
+        {
+            var product = await _storeContext.Products.FindAsync(id);
+
+            return product;
+            
+        }
+    }
+}
